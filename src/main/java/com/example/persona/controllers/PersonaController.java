c1f949ca68a2410b89268f5401e8b4e223d1758b
Gabriel <gabriel.kitanovich@gmail.com>
2024-09-21 19:16:49 -0300

    Base Repositories&Services + Implementations

--- src/main/java/com/example/persona/controllers/PersonaController.java
+++ src/main/java/com/example/persona/controllers/PersonaController.java
@@ -2,7 +2,7 @@ package com.example.persona.controllers;
 
 
 import com.example.persona.entities.Persona;
-import com.example.persona.services.PersonaService;
+import com.example.persona.services.PersonaServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +11,16 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/personas")
 public class PersonaController {
-    private PersonaService personaService;
-    public PersonaController(PersonaService personaService) {
-        this.personaService = personaService;
+    private PersonaServiceImpl personaServiceImpl;
+    public PersonaController(PersonaServiceImpl personaServiceImpl) {
+        this.personaServiceImpl = personaServiceImpl;
     }
 
 
     @GetMapping("")
     public ResponseEntity<?> getAll() {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.findAll());
+            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.findAll());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. No se encontraron Personas.\"}");
         }
@@ -29,7 +29,7 @@ public class PersonaController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getOne(@PathVariable Long id) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.findById(id));
+            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.findById(id));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. No se encontraron Personas con esa id.\"}");
         }
@@ -38,7 +38,7 @@ public class PersonaController {
     @PostMapping("")
     public ResponseEntity<?> save(@RequestBody Persona entity) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.save(entity));
+            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.save(entity));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. No se pudo guardar.\"}");
         }
@@ -47,7 +47,7 @@ public class PersonaController {
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable Long id,@RequestBody Persona entity) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.update(id, entity));
+            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.update(id, entity));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. No se pudo actualizar.\"}");
         }
@@ -56,7 +56,7 @@ public class PersonaController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         try {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(personaService.delete(id));
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(personaServiceImpl.delete(id));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. No se pudo eliminar.\"}");
         }
--- src/main/java/com/example/persona/entities/Autor.java
+++ src/main/java/com/example/persona/entities/Autor.java
@@ -1,29 +1,30 @@
 package com.example.persona.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.envers.Audited;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.io.Serializable;
 
 @Entity
-@Table(name = "persona")
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "autor")
 @Getter
 @Setter
-@EnableJpaAuditing
+@AllArgsConstructor
+@NoArgsConstructor
 @Audited
-public class Persona extends Base  {
+public class Autor extends Base{
 
     @Column(name = "nombre")
     private String nombre;
+
     @Column(name = "apellido")
     private String apellido;
-    @Column(name = "dni")
-    private int dni;
+
+    @Column(name = "biografia", length = 1500)
+    private String biografia;
 }
