52cf7919a1efa16138f451faff488496350468cf
Gabriel <gabriel.kitanovich@gmail.com>
2024-09-22 20:23:35 -0300

    Add relationships & Domicilio entity implementation

---
+++ src/main/java/com/example/persona/controllers/LocalidadController.java
@@ -0,0 +1,16 @@
+package com.example.persona.controllers;
+
+import com.example.persona.entities.Autor;
+
+import com.example.persona.entities.Localidad;
+import com.example.persona.services.AutorServiceImpl;
+
+import com.example.persona.services.LocalidadServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "api/v1/localidades")
+public class LocalidadController extends BaseControllerImpl<Localidad, LocalidadServiceImpl>{
+
+}
--- src/main/java/com/example/persona/entities/Persona.java
+++ src/main/java/com/example/persona/entities/Persona.java
@@ -9,6 +9,8 @@ import org.hibernate.envers.Audited;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "persona")
@@ -26,4 +28,17 @@ public class Persona extends Base  {
     private String apellido;
     @Column(name = "dni")
     private int dni;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_domicilio")
+    private Domicilio domicilio;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinTable(
+            name = "persona_libro",
+            joinColumns = @JoinColumn(name = "persona_id"),
+            inverseJoinColumns = @JoinColumn(name = "libro_id")
+    )
+    private List<Libro> libros = new ArrayList<Libro>();
+
 }
